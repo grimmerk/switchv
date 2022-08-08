02317d4b125b7275421339d430bbe8e59043d1cc
Grimmer <grimmer0125@gmail.com>
2022-08-09 00:39:50 +0800

    feat: improve server make via remove duplicate engine bak file

--- server/build.sh
+++ server/build.sh
@@ -1,4 +1,5 @@
 perl -i -lpe 'print " \"version\": \"0.0.1\"," if $. == 2'  ./node_modules/.prisma/client/package.json
 yarn build
+rm -rf ./node_modules/.prisma/client/libquery_engine-darwin-arm64.dylib.node.bak
 pkg --debug -t node16-macos-arm64 -o xwin-server-macos .
 sed -i -e  "2d"  ./node_modules/.prisma/client/package.json
