08af1ff3dacbe5260033cfe6ecff4930b1334663
Grimmer <grimmer0125@gmail.com>
2022-12-21 00:55:59 +0800

    fix(electron): intel mac build scripts

--- electron/clean.sh
+++ electron/clean.sh
@@ -1,3 +1,10 @@
 rm -rf ../node_modules
 
-rm node_modules/@prisma/engines/migration-engine-darwin 
+if [ -e node_modules/@prisma/engines/migration-engine-darwin-arm64 ]
+then
+    echo "migration darwin-arm64 exist, arm mac. delete darwin"
+    rm node_modules/@prisma/engines/migration-engine-darwin
+else
+
+    echo "migration arm64 no exist, is cpu mac"
+fi
--- electron/prebuild.sh
+++ electron/prebuild.sh
@@ -2,4 +2,10 @@ mkdir -p ../node_modules/@prisma/engines
 cp -r node_modules/@prisma/engines/dist ../node_modules/@prisma/engines/
 cp node_modules/@prisma/engines/package.json  ../node_modules/@prisma/engines/
 
-cp node_modules/@prisma/engines/migration-engine-darwin-arm64 node_modules/@prisma/engines/migration-engine-darwin 
+if [ -e node_modules/@prisma/engines/migration-engine-darwin-arm64 ]
+then
+    echo "migration arm64 exists. arm mac. copy darwin-arm64 as darin"
+    cp node_modules/@prisma/engines/migration-engine-darwin-arm64 node_modules/@prisma/engines/migration-engine-darwin 
+else
+    echo "migration arm64 does no exist, is cpu mac"
+fi
