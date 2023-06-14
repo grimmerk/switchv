84d1d62b1e0b2441cacd71ac32520c0534210972
Grimmer <grimmer0125@gmail.com>
2023-06-14 09:09:56 +0800

    chore(ElectronTeamID): update sign.sh

--- electron/sign.sh
+++ electron/sign.sh
@@ -1,1 +1,5 @@
 echo "try to sign app"
+
+# step1: add ElectronTeamID in INfo.plist 
+# https://www.electronjs.org/docs/latest/tutorial/mac-app-store-submission-guide#extra-steps-without-electron-osx-sign
+plutil -replace ElectronTeamID -string "GL35G6YCWG" /Users/grimmer/git/xwin/electron/out/SwitchV-mas-arm64/SwitchV.app/Contents/Info.plist
